1bcb0bfddee7ab38a2e2ba241439377ec2ee7f62
Micah <tr00datp00nar@gmail.com>
2023-03-13 12:17:02 -0400

    Use echo instead of printf

--- scripts/get_track_title.sh
+++ scripts/get_track_title.sh
@@ -7,5 +7,5 @@ full_title="$(playerctl --player=ncspot metadata -f "{{ title }}")"
 title=$(echo "$full_title" | sed -re 's/\(.*\).*$//')
 
 if [[ $status =~ Playing ]]; then
-    printf "%s "$title"
+   echo "$title"
 fi
