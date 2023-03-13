8900de70a583fa0614ce8a38c8e6a98a3399e402
Micah <tr00datp00nar@gmail.com>
2023-03-13 12:22:06 -0400

    Bug chasing

--- scripts/get_track_title.sh
+++ scripts/get_track_title.sh
@@ -6,6 +6,6 @@ full_title="$(playerctl --player=ncspot metadata -f "{{ title }}")"
 # Trims track title from any opening parentheses.
 # title=$(echo "$full_title" | sed -re 's/\(.*\).*$//')
 
-if [[ $status =~ Playing ]]; then
-  echo $full_title | sed -re 's/\(.*\).*$//'
+if [[ "$status" =~ Playing ]]; then
+  echo "$full_title" | sed -re 's/\(.*\).*$//'
 fi
