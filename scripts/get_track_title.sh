f4d1c8da08b97f23034de534fd3058d14ebf967f
Micah <tr00datp00nar@gmail.com>
2023-03-13 12:14:52 -0400

    Fix typo

--- scripts/get_track_title.sh
+++ scripts/get_track_title.sh
@@ -4,7 +4,7 @@ status="$(playerctl --player=ncspot metadata -f "{{ status }})"
 full_title="$(playerctl --player=ncspot metadata -f "{{ title }}")"
 
 # Trims track title from any opening parentheses.
-title=$(printf "%s" "$full_title" | sed -re 's/\(.*\).*$//)'
+title=$(printf "%s" "$full_title" | sed -re 's/\(.*\).*$//i')
 
 if [[ $status =~ Playing ]]; then
     printf "%s "$title"
